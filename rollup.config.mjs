c1655668ce792d92e1d0c048adcd796a094040f5
carbonsoda <carbonsoda@outlook.com>
2025-06-30 12:54:36 +0800

    workspace updates

---
+++ rollup.config.mjs
@@ -0,0 +1,45 @@
+import deletePlugin from "rollup-plugin-delete";
+
+import resolvePlugin from "@rollup/plugin-node-resolve";
+import terserPlugin from "@rollup/plugin-terser";
+import dtsPlugin from "rollup-plugin-dts";
+import ts2Plugin from "rollup-plugin-typescript2";
+
+export default [
+	{
+		input: "src/index.ts",
+		output: { file: "dist/index.esm.js", format: "esm" },
+
+		plugins: [
+			deletePlugin({ targets: "dist", runOnce: true }),
+
+			ts2Plugin({ useTsconfigDeclarationDir: true }),
+			resolvePlugin(),
+
+			terserPlugin(),
+		],
+
+		watch: true,
+	},
+	{
+		input: "src/index.ts",
+		output: { file: "dist/index.js", format: "cjs" },
+
+		plugins: [
+			ts2Plugin({ useTsconfigDeclarationDir: true }),
+			resolvePlugin(),
+
+			terserPlugin(),
+		],
+
+		watch: false,
+	},
+	{
+		input: "dist/types/index.d.ts",
+		output: [{ file: "dist/index.d.ts", format: "es" }],
+
+		plugins: [dtsPlugin()],
+
+		watch: true,
+	},
+];
