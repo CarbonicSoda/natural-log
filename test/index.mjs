ea866c2221678b7d435aa255ab8540c735b51efd
carbonsoda <carbonsoda@outlook.com>
2025-06-30 17:26:42 +0800

    remove tests

--- test/index.mjs
+++
@@ -1,8 +0,0 @@
-import { natlog } from "natural-log";
-
-natlog({ timeout: 9999 });
-console.log({ cause: 69, details: [null] });
-console.warn("FLAG", false);
-console.error(new SyntaxError("Brain Loss"));
-console.debug(new TextEncoder().encode("Hello World"));
-console.log(console, 1);
